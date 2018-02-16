59034c057e79225afdd6b7d7e3aedb77c4f4e238
pigney <yoann.pigne@univ-lehavre.fr>
2018-02-16 10:16:33 +0100

    headers + format

--- src/main/java/org/graphstream/httpsource/HTTPSource.java
+++ src/main/java/org/graphstream/httpsource/HTTPSource.java
@@ -1,16 +1,9 @@
 /*
- * Copyright 2006 - 2016
- *     Stefan Balev     <stefan.balev@graphstream-project.org>
- *     Julien Baudry    <julien.baudry@graphstream-project.org>
- *     Antoine Dutot    <antoine.dutot@graphstream-project.org>
- *     Yoann Pigné      <yoann.pigne@graphstream-project.org>
- *     Guilhelm Savin   <guilhelm.savin@graphstream-project.org>
- *
  * This file is part of GraphStream <http://graphstream-project.org>.
- *
+ * 
  * GraphStream is a library whose purpose is to handle static or dynamic
  * graph, create them from scratch, file or any source and display them.
- *
+ * 
  * This program is free software distributed under the terms of two licenses, the
  * CeCILL-C license that fits European law, and the GNU Lesser General Public
  * License. You can  use, modify and/ or redistribute the software under the terms
@@ -18,24 +11,36 @@
  * URL <http://www.cecill.info> or under the terms of the GNU LGPL as published by
  * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
  */
-package org.graphstream.httpsource;
+
+/**
+ * @since 2011-08-25
+ * 
+ * @author Guilhelm Savin <guilhelm.savin@graphstream-project.org>
+ * @author Sven Marquardt <Sven2q@hotmail.de>
+ * @author Hicham Brahimi <hicham.brahimi@graphstream-project.org>
+ */
+package org.graphstream.stream.net;
+
+import static spark.Spark.delete;
+import static spark.Spark.path;
+import static spark.Spark.port;
+import static spark.Spark.post;
 
 import org.apache.commons.lang3.math.NumberUtils;
 import org.graphstream.stream.SourceBase;
 
 import spark.Route;
-import spark.Spark;
 
 /**
  * This source allows to control a graph from a web browser. Control is done
@@ -57,7 +62,7 @@ import spark.Spark;
  * <li><code>/step/:step</code> post: take given steps</li>
  *
  * </ul>
- *
+ * 
  */
 public class HTTPSource extends SourceBase {
 
@@ -71,7 +76,7 @@ public class HTTPSource extends SourceBase {
 	 * Create a new http source. The source will be available on
 	 * 'http://localhost/graphId' where graphId is passed as parameter of this
 	 * constructor. Also this starts the server already
-	 *
+	 * 
 	 * @param graphId
 	 *            id of the graph
 	 * @param port
@@ -79,7 +84,7 @@ public class HTTPSource extends SourceBase {
 	 */
 	public HTTPSource(final String graphId, final int port) {
 		super(graphId);
-		Spark.port(port);
+		port(port);
 		this.graphId = graphId;
 		this.setupRoutes();
 	}
@@ -96,17 +101,17 @@ public class HTTPSource extends SourceBase {
 	 */
 	private void setupRoutes() {
 
-		Spark.path("/" + this.graphId, () -> {
-			Spark.path("/node", () -> {
-				Spark.post("/:id", this.addNode);
-				Spark.delete("/:id", this.deleteNode);
+		path("/" + this.graphId, () -> {
+			path("/node", () -> {
+				post("/:id", addNode);
+				delete("/:id", deleteNode);
 			});
-			Spark.path("/edge", () -> {
-				Spark.post("/:id/:from/:to/:directed", this.addEdge);
-				Spark.delete("/:id", this.deleteEdge);
+			path("/edge", () -> {
+				post("/:id/:from/:to/:directed", addEdge);
+				delete("/:id", deleteEdge);
 			});
-			Spark.path("/step", () -> {
-				Spark.post("/:step", this.takeStep);
+			path("/step", () -> {
+				post("/:step", takeStep);
 			});
 		});
 
@@ -115,8 +120,8 @@ public class HTTPSource extends SourceBase {
 	/**
 	 * Add Node
 	 */
-	private final Route addNode = (req, resp) -> {
-		this.sendNodeAdded(this.sourceId, req.params(":id"));
+	private Route addNode = (req, resp) -> {
+		this.sendNodeAdded(sourceId, req.params(":id"));
 		resp.status(200);
 		resp.type("text");
 		return resp;
@@ -124,8 +129,8 @@ public class HTTPSource extends SourceBase {
 	/**
 	 * Delete Node
 	 */
-	private final Route deleteNode = (req, resp) -> {
-		this.sendNodeRemoved(this.sourceId, req.params(":id"));
+	private Route deleteNode = (req, resp) -> {
+		this.sendNodeRemoved(sourceId, req.params(":id"));
 		resp.status(200);
 		resp.type("text");
 		return resp;
@@ -133,8 +138,8 @@ public class HTTPSource extends SourceBase {
 	/**
 	 * Add Edge
 	 */
-	private final Route addEdge = (req, resp) -> {
-		this.sendEdgeAdded(this.sourceId, req.params(":id"), req.params(":from"), req.params(":to"),
+	private Route addEdge = (req, resp) -> {
+		this.sendEdgeAdded(sourceId, req.params(":id"), req.params(":from"), req.params(":to"),
 				Boolean.getBoolean(req.params("directed")));
 		resp.status(200);
 		resp.type("text");
@@ -143,8 +148,8 @@ public class HTTPSource extends SourceBase {
 	/**
 	 * Delete Edge
 	 */
-	private final Route deleteEdge = (req, resp) -> {
-		this.sendEdgeRemoved(this.sourceId, req.params(":id"));
+	private Route deleteEdge = (req, resp) -> {
+		this.sendEdgeRemoved(sourceId, req.params(":id"));
 		resp.status(200);
 		resp.type("text");
 		return resp;
@@ -152,9 +157,9 @@ public class HTTPSource extends SourceBase {
 	/**
 	 * Take given steps
 	 */
-	private final Route takeStep = (req, resp) -> {
+	private Route takeStep = (req, resp) -> {
 		if (NumberUtils.isCreatable(req.params(":step"))) {
-			this.sendStepBegins(this.sourceId, Double.parseDouble(req.params(":step")));
+			this.sendStepBegins(sourceId, Double.parseDouble(req.params(":step")));
 			resp.status(200);
 			resp.type("text");
 			return resp;
--- src/test/java/org/graphstream/httpsource/TestHTTPSource.java
+++ src/test/java/org/graphstream/httpsource/TestHTTPSource.java
@@ -1,4 +1,38 @@
-package org.graphstream.httpsource;
+/*
+ * This file is part of GraphStream <http://graphstream-project.org>.
+ * 
+ * GraphStream is a library whose purpose is to handle static or dynamic
+ * graph, create them from scratch, file or any source and display them.
+ * 
+ * This program is free software distributed under the terms of two licenses, the
+ * CeCILL-C license that fits European law, and the GNU Lesser General Public
+ * License. You can  use, modify and/ or redistribute the software under the terms
+ * of the CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
+ * URL <http://www.cecill.info> or under the terms of the GNU LGPL as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
+ */
+
+/**
+ * @since 2017-04-15
+ * 
+ * @author Sven Marquardt <Sven2q@hotmail.de>
+ * @author Hicham Brahimi <hicham.brahimi@graphstream-project.org>
+ */
+package org.graphstream.stream.net.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 import java.io.IOException;
 
@@ -10,8 +44,7 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.Is;
+import org.graphstream.stream.net.HTTPSource;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -31,56 +64,56 @@ public class TestHTTPSource {
 	 */
 	@BeforeClass
 	public static void setUp() {
-		TestHTTPSource.source = new HTTPSource("g1", 8080);
-		TestHTTPSource.graph = new DefaultGraph("g1");
-		TestHTTPSource.source.addSink(TestHTTPSource.graph);
+		source = new HTTPSource("g1", 8080);
+		graph = new DefaultGraph("g1");
+		source.addSink(graph);
 	}
 
 	@Test
 	public void testAddAndDeleteNode() throws IOException {
 
-		HttpUriRequest request = new HttpPost(TestHTTPSource.ROOT_URL + TestHTTPSource.NODE_1);
+		HttpUriRequest request = new HttpPost(ROOT_URL + NODE_1);
 		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		MatcherAssert.assertThat(TestHTTPSource.graph.getNodeCount(), Is.is(1));
-		request = new HttpDelete(TestHTTPSource.ROOT_URL + TestHTTPSource.NODE_1);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(1));
+		request = new HttpDelete(ROOT_URL + NODE_1);
 		httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		MatcherAssert.assertThat(TestHTTPSource.graph.getNodeCount(), Is.is(0));
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(0));
 
 	}
 
 	@Test
 	public void testAddAndDeleteEdge() throws ClientProtocolException, IOException {
-		HttpUriRequest request = new HttpPost(TestHTTPSource.ROOT_URL + TestHTTPSource.NODE_1);
+		HttpUriRequest request = new HttpPost(ROOT_URL + NODE_1);
 		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		request = new HttpPost(TestHTTPSource.ROOT_URL + "/node/2");
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/node/2");
 		httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		request = new HttpPost(TestHTTPSource.ROOT_URL + "/edge/1/1/2/false");
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/edge/1/1/2/false");
 		httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		MatcherAssert.assertThat(TestHTTPSource.graph.getNodeCount(), Is.is(2));
-		MatcherAssert.assertThat(TestHTTPSource.graph.getEdgeCount(), Is.is(1));
-		request = new HttpDelete(TestHTTPSource.ROOT_URL + "/edge/1");
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(2));
+		assertThat(graph.getEdgeCount(), is(1));
+		request = new HttpDelete(ROOT_URL + "/edge/1");
 		httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
 	}
 
 	@Test
 	public void testStep() throws ClientProtocolException, IOException {
-		HttpUriRequest request = new HttpPost(TestHTTPSource.ROOT_URL + "/step/1");
+		HttpUriRequest request = new HttpPost(ROOT_URL + "/step/1");
 		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(200));
-		request = new HttpPost(TestHTTPSource.ROOT_URL + "/step/A");
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/step/A");
 		httpResponse = HttpClientBuilder.create().build().execute(request);
-		MatcherAssert.assertThat(httpResponse.getStatusLine().getStatusCode(), Is.is(400));
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(400));
 	}
 
 	@After
 	public void cleanGraph() {
-		TestHTTPSource.graph.clear();
+		graph.clear();
 	}
 
 	/*
@@ -88,7 +121,7 @@ public class TestHTTPSource {
 	 */
 	@AfterClass
 	public static void cleanUp() {
-		TestHTTPSource.source.stop();
+		source.stop();
 	}
 
 }
